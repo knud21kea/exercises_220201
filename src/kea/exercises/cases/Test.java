67627c71cad360c369caab7e8d58f293cc1e7c6c
knud21kea <grah0070@stud.kea.dk>
2022-02-01 22:52:28 +0100

    Cases added

---
+++ src/kea/exercises/cases/Test.java
@@ -0,0 +1,42 @@
+package kea.exercises.cases;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // Input a sentence and output with words converted upper/lower case
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> wordList;
+
+        System.out.println("Please enter a sentence");
+        String sentence = input.nextLine();
+        //String sentence = "To be or NOT to be that is tHE question"; //test data
+        wordList = splitInputToArrayList(sentence);
+        for (String word : wordList) {
+            System.out.print(convertWord(word) + " ");
+        }
+
+    }
+
+    private static ArrayList<String> splitInputToArrayList(String sentence) {
+        return new ArrayList<>(Arrays.asList(sentence.split(" ")));
+    }
+
+    private static String convertWord(String word) {
+        String convertedWord;
+        if (Objects.equals(word, word.toUpperCase())) {
+            convertedWord = word; // word is all upper case so no change
+        } else {
+            convertedWord = word.toLowerCase(); // all other words to lower case
+            if (word.length() >3) {
+                convertedWord = word.substring(0,1).toUpperCase() + word.substring(1); // 1st letter upper case
+            }
+        }
+        return convertedWord;
+    }
+}
--- src/kea/exercises/grass/Grass_unused.java
+++ src/kea/exercises/grass/Grass_unused.java
@@ -2,13 +2,13 @@ package kea.exercises.grass;
 
 // Not needed
 
-public class Grass {
+public class Grass_unused {
 
     private final double lengthNow;
     private final double lengthMax;
     private final double growth;
 
-    public Grass(double lengthNow, double lengthMax, double growth) {
+    public Grass_unused(double lengthNow, double lengthMax, double growth) {
         this.lengthNow = lengthNow;
         this.lengthMax = lengthMax;
         this.growth = growth;
